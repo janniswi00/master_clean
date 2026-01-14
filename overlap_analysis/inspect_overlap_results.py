eae7bc21624cf25af805f6e1967ebc8e323f9734
jannis <jannis.witte2000@gmail.com>
2026-01-14 16:36:15 +0000

    added toggle bar and registered images

--- overlap_analysis/inspect_overlap_results.py
+++ overlap_analysis/inspect_overlap_results.py
@@ -155,6 +155,9 @@ from starfish.image import ApplyTransform, LearnTransform, Filter
 import json
 import glob
 import pandas as pd
+import networkx as nx
+from magicgui import magicgui
+from napari.layers import Image
 
 
 ## Arguments
@@ -199,19 +202,86 @@ for fov in fovs:
 viewer = napari.Viewer()
 
 colors = ["red", "blue", "green", "yellow", "pink", "magenta", "brown", "gray", "cyan", "lime", "purple", "orange"]
+scale = [px_size_y, px_size_x]
+translates = []
 
 for mp, mp_idx, c in zip(mps, range(0, len(mps)), colors[:len(mps)]):
-    x0 = mp.xc[0]
-    y0 = mp.yc[0]
-
+    x0 = mp.xc.values[0]
+    y0 = mp.yc.values[0]
     translate = [y0, x0]
-    scale = [px_size_y, px_size_x]
-    viewer.add_image(mp, name=f" mp{mp_idx}", colormap=c, blending="additive", contrast_limits=[0, 0.4], visible=True, translate=translate, scale=scale)
+    translates.append(translate)
+
+    viewer.add_image(mp, name=f"mp{mp_idx}", colormap=c, blending="additive", contrast_limits=[0, 0.4], visible=False, translate=translate, scale=scale)
 
 ## Load MST and registration data
 regs = pd.read_csv("edges_and_registration.csv").query("strain==@strain & rep==@rep & hpi==@hpi")
-print(regs)
+edges = [(source, target, weight) for source, target, weight in zip(list(regs["source"]), list(regs["target"]), list(regs["weight"]))]
 
+# Identify connected MSTs and root for each MST
+G = nx.Graph()
+G.add_weighted_edges_from(edges)
 
+for mst in nx.connected_components(G):
+    sub = G.subgraph(mst)
 
+    node_weight_sum = {
+        n: sum(d.get("weight", 0) for _, _, d in sub.edges(n, data=True))
+        for n in sub.nodes
+    }
+
+    root = list(node_weight_sum.keys())[list(node_weight_sum.values()).index(max(node_weight_sum.values()))]
+    viewer.add_image(mps[root], name=f" mp{root} reg", colormap=colors[root], blending="additive", contrast_limits=[0, 0.4], visible=True, translate=translates[root], scale=scale)
+    tree = nx.bfs_tree(sub, root)
+
+    branches = {
+        child: list(nx.descendants(tree, child)) + [child]
+        for child in tree.successors(root)
+    }
+
+    print(regs)
+    print("root: ", root)
+
+    for branch in branches.items():
+        print("branch: ", branch)
+        nodes = branch[1][::-1]
+        prev_node = root
+        tx = 0
+        ty = 0
+
+        for node in nodes:
+            print("node: ", node)
+            print("prev_node: ", prev_node)
+            print(regs.query("source==@prev_node & target==@node"))
+
+            source_target_df = pd.concat([regs.query("source==@prev_node & target==@node"), regs.query("source==@node & target==@prev_node")])
+
+            print("len_df: ", len(source_target_df))
+
+            if len(source_target_df) < 1:
+                print("WARNING: more than one entries for source-target pair")
+
+            tx += source_target_df["tx"].values[0]
+            ty += source_target_df["ty"].values[0]
+
+            print("Registration: ", tx, ty)
+
+            trans = translates[node]
+            trans_y = trans[0] + ty
+            trans_x = trans[1] + tx
+
+            print("Translate: ", trans)
+
+            viewer.add_image(mps[node], name=f" mp{node} reg", colormap=colors[node], blending="additive", contrast_limits=[0, 0.4], visible=True, translate=[trans_y, trans_x], scale=scale)
+            prev_node = node
+
+@magicgui(call_button="Apply Registration")
+def toggle_images():
+    for layer in viewer.layers:
+        if isinstance(layer, Image) and "reg" in layer.name:
+            layer.visible = not layer.visible
+
+        if isinstance(layer, Image) and "reg" not in layer.name:
+            layer.visible = not layer.visible
+
+viewer.window.add_dock_widget(toggle_images)
 napari.run()
